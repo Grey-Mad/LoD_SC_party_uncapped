a0dc82654fb063baec29a6bc4d9829ac6e3d1822
Grey-Mad <greypmadison@gmail.com>
2024-12-06 19:29:21 -0500

    Looped char xp tables

---
+++ src/main/java/legend/game/characters/CharacterNames.java
@@ -0,0 +1,15 @@
+package legend.game.characters;
+
+import org.legendofdragoon.modloader.registries.RegistryEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CharacterNames extends RegistryEntry {
+   public CharacterNames() {} 
+   public List<String> names = new ArrayList<>();
+   
+
+    
+      
+}
